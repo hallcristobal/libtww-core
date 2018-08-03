6ef3bc88118ce0719d07f5492f0a021d32c5a5ea
Cristobal Hall-Ramos <crishallramos@yahoo.com>
2018-08-03 19:07:55 -0400

    Fix OOM with alloc feature

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,13 +1,13 @@
 #![allow(private_no_mangle_fns)]
-
+#[cfg(feature = "alloc")]
+use core_alloc::alloc::Layout;
+#[cfg(feature = "alloc")]
 #[lang = "oom"]
-#[no_mangle]
-pub fn rust_oom() -> ! {
+fn rust_oom(_: Layout) -> ! {
     loop {}
 }
 
 use core::panic::PanicInfo;
-
 #[panic_implementation]
 #[no_mangle]
 pub fn my_panic(_: &PanicInfo) -> ! {
